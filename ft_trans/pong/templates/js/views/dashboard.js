5586aea0c9588adca69535e09e36390a2c812ae0
faperac <fabricio.pereira.acuna@gmail.com>
2024-09-17 16:29:19 +0200

    update button play

--- ft_trans/pong/templates/js/views/dashboard.js
+++ ft_trans/pong/templates/js/views/dashboard.js
@@ -83,11 +83,9 @@ export function dashboard(player_name) {
 				<div class="col-md-9 main-content">
 					<div class="card">
 						<div class="card-body">
-							<div class="btn-group" id="play_button" "role=" group" aria-label="Basic example">
-								<button type="button" id="game_alone" class="btn btn-primary">Play</button>
-								<button type="button" id="game_friend" class="btn btn-primary">Play with friend</button>
-								<button type="button" id="game_tournament" class="btn btn-primary">Play tournament</button>
-							</div>
+						<button type="button" class="btn btn-primary btn-lg" id="game_alone">
+							Play
+						</button>
 						</div>
 					</div>
 					<div class="card">
@@ -207,7 +205,7 @@ function openPrivateChat(friendName) {
 	inputContainer.className = 'input-container';
 	inputContainer.innerHTML = `
 		<input type="text" id="message-input-${friendName}" placeholder="Type your message...">
-		<button onclick="sendPrivateMessage('${friendName}')">Send</button>
+		<button onclick="Message('${friendName}')">Send</button>
 	`;
 	chatContainer.appendChild(inputContainer);
 
@@ -236,12 +234,6 @@ function setupDashboardEvents(navigateTo, player_name) {
 		console.log('Play game_alone button clicked');
 
 		window.location.href = 'http://localhost:4000';
-	});
-
-	//Game with friend
-	document.getElementById('game_friend').addEventListener('click', function (event) {
-		event.preventDefault();
-		console.log('Play game_friend button clicked');
 	});
 
 	// Friend menu
