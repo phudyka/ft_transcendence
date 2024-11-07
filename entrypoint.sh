64edd98d33c8b1f7c8afb96e6516651921c3d576
Edson Firmino <efirmino@c1r4p2.42nice.fr>
2024-11-07 10:41:00 +0100

    vide

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Appliquer les migrations
-python manage.py makemigrations
-python manage.py migrate
-
-# # Démarrer le serveur Django
-# exec "$@"
--- ft_trans/pong/templates/js/game/README
+++
@@ -1,4 +0,0 @@
-lancement:
-
-docker build -t pong .
-docker run -p 3000:3000 pong
